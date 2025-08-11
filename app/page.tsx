d6e5dd6d0e51bdcb6828a45cc288398bf5842f80
travisc671 <travisc088@gmail.com>
2025-08-11 00:02:53 -0500

    refactored code, moved from react pinch zoom to a custom component
    got the hid labels to work, but it could still use some help with
    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,15 @@
 "use client";
-
-import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import Connect from "./lib/connect";
 import Keyboard from "./lib/keyboard";
+import CanvasPan from "@/components/canvas-pan";
 
 export default function Home() {
   return (
     <div className="">
       <Connect />
-      <TransformWrapper limitToBounds={false} minScale={0.25} maxScale={2.5}>
-        <TransformComponent
-          wrapperStyle={{
-            width: "100vw",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Keyboard />
-        </TransformComponent>
-      </TransformWrapper>
+      <CanvasPan className="h-screen w-screen">
+        <Keyboard />
+      </CanvasPan>
     </div>
   );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -1,21 +1,28 @@
 import { RpcConnection } from "@zmkfirmware/zmk-studio-ts-client";
-import { PhysicalLayout } from "@zmkfirmware/zmk-studio-ts-client/keymap";
+import {
+  Keymap,
+  PhysicalLayout,
+} from "@zmkfirmware/zmk-studio-ts-client/keymap";
 import { create } from "zustand";
 
 type RpcStore = {
   rpcConnection: RpcConnection | undefined;
   physicalLayouts: PhysicalLayout[] | undefined;
   selectedLayout: number | undefined;
+  keyMap: Keymap | undefined;
   setRpcConnection: (connection: RpcConnection) => void;
   setPhysicalLayouts: (layouts: PhysicalLayout[]) => void;
-  setSelectedLayout: (index: number) => void; 
+  setSelectedLayout: (index: number) => void;
+  setKeymap: (keymap: Keymap) => void;
 };
 
 export const useRpcStore = create<RpcStore>((set) => ({
   rpcConnection: undefined,
   physicalLayouts: undefined,
   selectedLayout: undefined,
+  keyMap: undefined,
   setRpcConnection: (connection) => set({ rpcConnection: connection }),
   setPhysicalLayouts: (layouts) => set({ physicalLayouts: layouts }),
-  setSelectedLayout: (index) => set({selectedLayout: index})
+  setSelectedLayout: (index) => set({ selectedLayout: index }),
+  setKeymap: (keymap) => set({ keyMap: keymap }),
 }));
